fe4b9736c4b99a4dddb9eb7269e5d631d568869e
AngeloTripodi <tripodi.angelo@gmail.com>
2022-12-05 18:48:17 +0100

    Completed task + bonus 1 + bonus 2

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+// const nameElement = document.getElementById('name');
+// const roleElement = document.getElementById('role');
+// const imgElement = document.getElementById('img');
+const divElement = document.getElementById('divCard');
+
+
 const teamList = [
     {
         name : "Wayne Barnett",
@@ -5,7 +11,7 @@ const teamList = [
         img : "wayne-barnett-founder-ceo.jpg"
     },
     {
-        name : "Wayne Barnett",
+        name : "Angela Caroll",
         role: "Chief Editor",
         img : "angela-caroll-chief-editor.jpg"
     },
@@ -35,5 +41,35 @@ const teamList = [
     for ( let i=0; i<teamList.length; i++)
 {   
     const teamListElement = teamList[i];
-    console.log(teamListElement.name, teamListElement.role, teamListElement.img)
+    // console.log(teamListElement.name, teamListElement.role, teamListElement.img)
+    // const newH1Element = document.createElement('h1');
+    // const newImgElement = document.createElement('img');
+    //!! newImgElement.innerHTML=`<img src="./img/${teamListElement.img}" alt="">`;
+    // newH1Element.innerText = teamListElement.name;
+    // nameElement.append(newH1Element);
+
+
+
+    const cardElement = document.createElement('div');
+    cardElement.innerHTML=`
+    <div class="col mb-4">
+                <div class="card shadow-sm">
+                  
+                  <img src="./img/${teamListElement.img}" alt="">
+                  <div class="card-body">
+                    <h1 id="name">
+                        ${teamListElement.name}
+                    </h1>
+                    <p class="text-info" id="role">${teamListElement.role}</p>
+                    <div class="d-flex justify-content-between align-items-center">
+                      <div class="btn-group">
+                        <button type="button" class="btn btn-sm btn-outline-secondary">Call</button>
+                        <button type="button" class="btn btn-sm btn-outline-secondary">Contact</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+    `;
+    divElement.append(cardElement);
 }
